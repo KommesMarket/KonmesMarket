94331d9f922bf799ee5636ad89de7f04c798f08a
Grimmjow Jaggerjack <mrcaphasie.m@gmail.com>
2018-11-16 16:44:19 -0400

    Products home

--- js/index.js
+++ js/index.js
@@ -12,3 +12,31 @@ function carousel() {
     x[slideIndex-1].style.display = "block"; 
     setTimeout(carousel, 2000); 
 }
+
+
+var cardDeck = document.getElementsByClassName('card-deck')[0];
+var data = [{ img: 
+    "./img/b.jpg", 
+    title: "Test titre", 
+    para: "Encore tu test", 
+    smallText: "Rien que du test",
+    Price: 50 + " €" }, 
+    { img: "./img/b.jpg", 
+    title: "Test titre", 
+    para: "Toujours du test", 
+    smallText: "Capich ?",
+    Price: 250 + " €"}];
+
+cardDeck.innerHTML += data.map(function(el, index, arr) {
+    return "<div class='card'><img class='card-img-top' src=" 
+    + el.img + 
+    " alt='Card image cap'><div class='card-body'><h4 class='card-title' > " 
+    + el.title +
+    "</h4 ><p class='card-text' > " 
+    + el.para +
+    "</p ><p class='card-text' > <small class='text-muted'>" 
+    + el.smallText +
+    "</small></p ><p class='card-text' > " 
+    + el.Price +
+    "</p ></div ></div >"
+})
